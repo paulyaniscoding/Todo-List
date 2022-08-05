fca2ab9d4d45312482cc909f6d5d37b59fafa93b
rdname <rdname@rdname.hk>
2022-08-05 18:57:39 +0800

    Add Sorting Logic Seudo Code

--- src/features/todos/TodosDemo.js
+++ src/features/todos/TodosDemo.js
@@ -65,6 +65,56 @@ export const TodosDemo = () => {
         }
     );
 
+    /*
+        Sorting Logic:
+            target:
+                root => rootChildren
+
+                // from itmRoots to organized JSX
+                getOrganizedItmByCategory = (unSorted_itms=rootChildren) => {
+                    // Sorting Phase
+                    unSorted_itms => {
+                        todoCategoryRootList = {
+                            category: [rootList],
+                        }
+                        sortedCategory = todoCategoryRootList => sortedCategory
+                        return [sortedCategory, todoCategoryRootList]
+                    }
+
+                    // Render Phase
+                    itmsOrganizedByTodoCategory = sortedCategory.map(
+                        category => {
+                            itmsUnderSameCategory = todoCategoryRootList[category]
+
+                            // Get itmJSXsUnderSameCategory
+                            init itmJSXsUnderSameCategory
+                            // Node Stage, run next sorting callback
+                            if (有下一個Sorting Callback) {
+                                (二次Sorting可以喺e個位做)
+                                (sorted_itmsUnderSameCategory = 二次SortingCallback(itmsUnderSameCategory))	
+                                (eg:)
+                                itmJSXsUnderSameCategory = getOrganizedItmByDate(itmsUnderSameCategory)
+                            } 
+                            // Leaf Stage, no next sorting callback
+                            else {
+
+                                // Render Itm						
+                                itmJSXsUnderSameCategory = itmsUnderSameCategory.map(
+                                    id => <TodoItem/>
+                                )
+                            }
+
+                            // Render Category
+                            return (
+                                <h2>
+                                    {itmJSXsUnderSameCategory}
+                                </h2>
+                            )
+                        }
+                    )
+                }        
+    */
+
     return (
         <>
             <AddTodoForm parentId={'root'} formLayout={<AddTodoFormInlineLayout/>}/>
