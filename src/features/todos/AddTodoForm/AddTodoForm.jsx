a6fabbf46ac2fc3acde139fbc2d553ad5ec02ff2
rdname <rdname@rdname.hk>
2022-08-05 18:20:30 +0800

    fix AddTodo

--- src/features/todos/AddTodoForm/AddTodoForm.jsx
+++ src/features/todos/AddTodoForm/AddTodoForm.jsx
@@ -29,11 +29,66 @@ export const AddTodoForm = ({ parentId, defaultTitle, formLayout }) => {
     const [content, setContent] = useState('');
     const [requiredTime, setRequiredTime] = useState('');
 
-    // Event Handlers
+    // Event Handlers {
     const onCategoryChanged = e => setCategory(e.target.value)
     const onTitleChanged = useCallback(e => {setTitle(e.target.value);}, []);
 
+    // onAddTodoClicked Handler
     const isInlineForm = parentId !== 'root';
+    // Adjust Category, cause there is no category input in inline form
+    let adjustedCategory = isInlineForm ? todosEntities[parentId].category : category;
+    let todoCategories = [...new Set(allTodos.filter(itm => itm.category).map(item => item.category))];
+    const todoCategoryOptions = todoCategories.map(category => (
+        <option
+            key={category}
+            value={category}
+        >
+            {category}
+        </option>
+    ));
+    const canAdd =
+        [adjustedCategory, title].every(Boolean)
+    const onAddTodoClicked = () => {
+        if (canAdd) {
+            //console.log('addTodo Args:', `${adjustedCategory}, ${title}, ${content}, ${requiredTime}, ${parentId}`)
+            dispatch(addTodo(adjustedCategory, title, content, requiredTime, parentId));
+            setCategory('');
+            setTitle('');
+            setContent('');
+            setRequiredTime('');
+        }
+    }
+
+    // onUpdateTodoClicked Handler
+    const canUpdate = title && title !== defaultTitle;
+    const onUpdateTodoClicked = (todoId) => {
+        if (canUpdate) {
+            dispatch(updateTodo({ id: todoId, title }));
+        }
+    }
+    // } Event Handlers
+
+
+    /* 好似諗錯嘢, Commented
+    @root
+        同Category, parent=root (implement咗)
+            => 無衝突
+        同Category, parent=其他 (無implement到)
+            => 唔合理
+        唔同Category, parent = root(implement咗)
+            => 更加無衝突
+        唔同Category, parent = 其他 (無implement到)
+            => 唔合理
+        => 要整走adjusted parent
+    @inline
+        無得set category
+            parent adjust嘅話
+                => 唔合理
+        => 無留adjusted parent嘅必要
+    @edit
+        無理parent
+            => 唔洗理
+
     // Adjust Parent Id
     let adjustedParentId = parentId;
     if (!isInlineForm) {
@@ -46,63 +101,29 @@ export const AddTodoForm = ({ parentId, defaultTitle, formLayout }) => {
         let categoryExists = Object.keys(existingCategories).includes(category);
         adjustedParentId = categoryExists ? existingCategories[category] : parentId;
     }
-    // Adjust Category
-    let adjustedCategory = isInlineForm ? todosEntities[parentId].category : category;
-
-
-
-    // TODO: 要check
-    const canAdd =
-        [adjustedCategory, title].every(Boolean)
-    const onAddTodoClicked = () => {
-        if (canAdd) {
-            //console.log('addTodo Args:', `${adjustedCategory}, ${title}, ${content}, ${requiredTime}, ${parentId}`)
-            dispatch(addTodo(adjustedCategory, title, content, requiredTime, adjustedParentId));
-            setCategory('');
-            setTitle('');
-            setContent('');
-            setRequiredTime('');
-        }
-    }
-
-    const canUpdate = title && title !== defaultTitle;
-    const onUpdateTodoClicked = (todoId) => {
-        if (canUpdate) {
-            dispatch(updateTodo({id:todoId, title}));
-        }
-    }
-
-    let todoCategories = [...new Set(allTodos.filter(itm => itm.category).map(item => item.category))];
-    const todoCategoryOptions = todoCategories.map(category => (
-        <option 
-            key={category} 
-            value={category} 
-        >
-            {category}
-        </option>
-    ));
-
-    const formLayoutProps = {
-        isInlineForm,
-        todoCategoryOptions,
-        category: adjustedCategory,
-        onCategoryChanged,
-        title,
-        onTitleChanged,
-        canAdd,
-        onAddTodoClicked,
-        canUpdate,
-        onUpdateTodoClicked,
-    }
-
-    const clonedFormLayout = React.cloneElement(
+    */
+   
+    const formLayoutWifFormProps = React.cloneElement(
         formLayout,
-        {formProps: formLayoutProps}
+        {
+            formProps: {
+                isInlineForm,
+                todoCategoryOptions,
+                category: adjustedCategory,
+                onCategoryChanged,
+                title,
+                onTitleChanged,
+                canAdd,
+                onAddTodoClicked,
+                canUpdate,
+                onUpdateTodoClicked,
+            }
+        }
     )
 
     return (
         <>
-            {clonedFormLayout}
+            {formLayoutWifFormProps}
         </>
     )
 }
